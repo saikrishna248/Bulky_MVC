76c113d13340596ea5d8b37c01817b721e69ba09
sai krishna <saikrishna15006@gmail.com>
2025-01-08 21:16:51 +0530

    New Pages For delete and Edit added

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using BulkyWeb.Data;
 using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWeb.Controllers
 {
@@ -38,9 +39,72 @@ namespace BulkyWeb.Controllers
             { 
             _context.Categories.Add(obj);        //  This is used to add the data in the table
             _context.SaveChanges();              // This is used to save the data in the database
-            return  RedirectToAction("Index"); // This is used to redirect to the Index page and controller is CategoryController
+            TempData["Success"] = "Data has been Created successfully"; // This is used to show the message after updating the data
+            return RedirectToAction("Index"); // This is used to redirect to the Index page and controller is CategoryController
             }
             return View();
         }
+        public IActionResult Edit(int? ID)       // This is used to create the data
+        {
+            if (ID == null || ID == 0)
+            {
+                return NotFound();
+            }
+            Category? obj = _context.Categories.Find(ID);      // This is used to get the record from the table
+          //  Category? obj1 = _context.Categories.FirstOrDefault(u=>u.ID ==ID);       // This is used to get the first record from the table
+        //    Category? obj2 = _context.Categories.Where(u=>u.ID==ID).FirstOrDefault();// This is used to get the first record from the table
+            if (obj == null) 
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(obj);        //  This is used to update the data in the table
+                _context.SaveChanges();              // This is used to save the data in the database
+                TempData["Success"] = "Data has been updated successfully"; // This is used to show the message after updating the data
+                return RedirectToAction("Index"); // This is used to redirect to the Index page and controller is CategoryController
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? ID)       // This is used to create the data
+        {
+            if (ID == null || ID == 0)
+            {
+                return NotFound();
+            }
+            Category? obj = _context.Categories.Find(ID);      // This is used to get the record from the table
+                                                               //  Category? obj1 = _context.Categories.FirstOrDefault(u=>u.ID ==ID);       // This is used to get the first record from the table
+                                                               //    Category? obj2 = _context.Categories.Where(u=>u.ID==ID).FirstOrDefault();// This is used to get the first record from the table
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost , ActionName("Delete")]
+        public IActionResult DeleteHere(int? ID)
+        {
+           
+            Category? obj = _context.Categories.Find(ID);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _context.Categories.Remove(obj);        //  This is used to delete the data in the table
+            _context.SaveChanges();              // This is used to save the data in the database
+            TempData["Success"] = "Data has been deleted successfully";
+            return RedirectToAction("Index"); // This is used to redirect to the Index page and controller is CategoryController
+
+          }
     }
 }
